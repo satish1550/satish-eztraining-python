aedde3281c93dd7e529bb13be1b8ee75649ac7db
Satish1550 <satishsunny1550@gmail.com>
2023-02-25 23:51:02 +0530

    PythonTraining II

---
+++ Techinal Training Phase II/Day1.py
@@ -0,0 +1,294 @@
+# DAY 1
+
+
+'''Lambda Function'''
+# when we want to use function concept without using function name there we use lambda funcution
+
+L = [1, 2, 3]
+ans = list(map(lambda i:i+i, L))
+print(ans)
+ans = list(map(lambda i:i*i, L))
+print(ans)
+
+res = list(map(lambda n: pow(n,2), L))
+print(res)
+
+str = 'Satish'
+(lambda str:print(str)) (str)
+
+
+
+# Writr a program after creating a list with even numbers with in range range 1-15, now using lambda functions to create a new list which should have sqare roots?
+List = []
+for i in range(1,16):
+    if i % 2 == 0:
+        List.append(i)
+print(List)
+res = map(lambda res: pow(res,1/2),List) # 1/2 = sqrt
+print(list(res))
+
+
+
+
+'''OOPs'''
+    # 1 Abstraction   --> Hiding data
+
+    # 3 Inheritance   --> Base class and Derived class
+            # Base     --> Parent class
+            # Derived  --> Child class
+            # Types:
+                # 1 Single inheritance
+                # 2 Multiple inheritance
+                # 3 Multi-level inheritance
+                # 4 Heirarichel inheritance
+                # 5 Hybrid inheritance
+
+    # 3 Encapsulation --> Bindind data and function into a single entity (for data security)
+            # Publi     --> One class info can be accessed by any othe class
+            # Private   --> Can used where it is declared or no inheritected class
+            # Protected --> Can be accessed only where it got declared...which ever class inherited from this class there also we can use
+
+    # 4 Polymorphism  --> One item or same item use for differnt purposses
+           # Typs
+               # 1 Overloading
+                   # 1 Operating overloading  --> Same operaton use for differnt opeartions (like '+' is for addtions of two no. as well as for concation of two srtings.)
+                   # 2 Method overloading     --> Same function used for differnt ways
+               # 2 Overriding --> If a method is cant not be used inside Derived class it will take it from Base clss or Parent class
+
+
+
+'''Abstraction'''
+from abc import ABC, abstractmethod
+
+class abstractdemo(ABC):
+    @abstractmethod # it is called the decorator to make the method abstract
+    def dispay (self):
+        None
+    @abstractmethod
+    def show(self):
+        None
+# changing abstract to concrete
+class demo(abstractdemo):
+    def dispay(self):
+        return print("Abstraction method")
+    def show(self):
+        return print("second function")
+
+object = demo()
+object.dispay()
+object.show()
+
+
+
+'''Inheritance'''
+class parent: # BASE Class
+    def disply(self):
+        print('parent class')
+class child(parent): # DERIVED Class
+    def show(self):
+        print('child class')
+
+object = child()
+object.disply()
+object.show()
+
+
+
+# single inheritance
+class A:
+    n = 30
+class B(A):
+    def calc(self):
+        res = self.n+70
+        print(res)
+object = B()
+object.calc()
+
+
+
+# multiple inheritance
+class mom:
+    def mDisply(self):
+        print("mom's class")
+class dad:
+    def dDisply(self):
+        print("dad's class")
+class child(mom, dad):
+    def cDisply(self):
+        print("child class")
+
+object = child()
+object.mDisply()
+object.dDisply()
+object.cDisply()
+
+
+
+# multi-level inheritance
+class grandParent:
+    def gDisply(self):
+        print("Grandparent's class")
+class parent(grandParent):
+    def pDisply(self):
+        print("Parent's class")
+class child(parent): # child is inheritance from parent function and parent is inheritance from grandParent
+    def cDisply(self):
+        print("child class")
+
+object = child()
+object.gDisply()
+object.pDisply()
+object.cDisply()
+
+
+
+# heirarichel inheritance
+class parent:
+    def pDisply(self):
+        print("Parent's class")
+class child1(parent):
+    def c1Disply(self):
+        print("first child class")
+class child2(parent): 
+    def c2Disply(self):
+        print("second child class")
+
+object = child1()
+object.pDisply()
+object.c1Disply()
+# object.c2Disply() # unable access the child2 class
+
+object1 = child2()
+object1.pDisply()
+object1.c2Disply()
+# object.c1Disply()  # unable access the child1 class
+
+
+
+# hybrid inheritance
+class parent:
+    def pDisplay(self):
+        print("Parent's class")
+class child1(parent):
+    def c1Display(self):
+        print("child1 class")
+class child2(parent):
+    def c2Display(self):
+        print("child2 class")
+class kid1(child1):
+    def k1Display(self):
+        print("kid1 class")
+class kid2(child1):
+    def k2Display(self):
+        print("kid2 class")
+class son1(child2):
+    def s1Display(self):
+        print("son1 class")
+class son2(child2):
+    def s2Display(self):
+        print("son2 class")
+
+o1 = child1()
+o1.pDisplay()
+o1.c1Display()
+
+o2 = child2()
+o2.pDisplay()
+o2.c2Display()
+
+o3 = kid1()
+o3.pDisplay()
+o3.c1Display()
+o3.k1Display()
+
+o4 = kid2()
+o4.pDisplay()
+o4.c1Display()
+o4.k2Display()
+
+o5 = son1()
+o5.pDisplay()
+o5.c2Display()
+o5.s1Display()
+
+o6 = son2()
+o6.pDisplay()
+o6.c2Display()
+o6.s2Display()
+
+
+
+
+'''Encapsulation'''
+# protected
+
+
+
+# Privaate 
+class encap:
+    __a = 10
+    def encapFunction(self):
+        print("EncapFunction")
+        print(self.__a)
+object = encap()
+object.encapFunction()
+
+
+
+'''Polymorphism'''
+
+'''Overloading'''
+
+# Operator overloading
+a = 10
+b = 20
+sum = a + b
+print("Here '+' is used for addition and sum is, ", sum)
+s1 = "Satish"
+s2 = "Daraboina"
+concat = s1 +" "+ s2
+print("Here '+' is used for concatination and res is, ", concat)
+
+
+
+# Method overloading
+class moverloading:
+    def display(self, a = None, b = None):
+        print(a,b)
+        
+obj = moverloading()
+print('without any arguments')
+obj.display()
+
+print('with all arguments')
+obj.display(10, 20)
+
+print('with one argiments')
+obj.display(10)
+
+
+
+'''Overriding'''
+class vijayawada():
+    def placeName(self):
+        print("vijayawada is know as KLU")
+    def student(self):
+        print("yes - Vijayawada")
+    def year(self):
+        print("IIIrd year")
+        
+class hyd():
+    def placeName(self):
+        print("Hyd is know as HYD-KLU")
+    def student(self):
+        print("yes - Hyd")
+    def year(self):
+        print("IIIrd year")
+
+obj_vij = vijayawada()
+obj_hyd = hyd()
+
+for details in (obj_vij, obj_hyd):
+    details.placeName()
+    details.student()
+    details.year()
---
+++ Techinal Training Phase II/Day2.py
@@ -0,0 +1,245 @@
+# DAY 2
+
+
+'''DSA'''
+    # Data Structures and Algorithms
+    # It helps to write the efficent programs
+    # Linear --> Arrays, linked List , Stack, Queue, Matrix
+    # Non Linear --> Binary Tree, Heap, Hash Table, Graph
+
+# Data Structures
+    # Linear:
+        # Statc --> Array (Fixed Size)
+        # Dynamic --> List, Stack, Queue (No Fixed Size)
+    # Non Linear:
+        # Graph
+        # Tree
+
+
+
+'''LINKED LIST'''
+    # real time example: Train
+    # As the name says link with one and another is called Linked List
+    # Types:
+        # Single Linked List / Singly
+        # double Linked List / Doubly
+        # ctrcular Linked List
+    # Opeartions:
+        # Insert
+            # At the Begining
+            # At the End
+            # At the Position
+        # Delete
+        # Search
+
+
+# Crating a Single Linked list
+    # step 1 --> create a Node
+    # step 2 --> parteshion the Node with two sigments Data and None
+    # step 3 --> Add value into the blank Node
+    # step 4 --> Mark the Node as Head
+    # step 5 --> Create the next Node by following the above stpe
+    # step 6 --> Establish the link between the first Node and Second Node
+
+# Displaying the Linked List:
+    # Traveasy is required to from first Node till Node in order to display linked list
+
+
+# Ex 1:
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class singleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def display(self):
+        if self.head is None:
+            print("List is empty..!")
+        else:
+            temp = self.head # temp = first Node
+            while temp:
+                print(temp.data, "->", end=" ") #
+                temp = temp.next # establishing ink the first Node to next Node
+                
+obj = singleLinkedList()
+# Node creation / Initialising
+n = Node(10) # n.data in Node class will be 10
+obj.head = n # Assigning first Node as Head
+n1 = Node(20)
+obj.head.next = n1 # next Node value
+n2 = Node(30)
+n1.next = n2
+obj.display()
+
+
+
+# Ex 2:
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+class singleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def display(self):
+        if self.head is None:
+            print("List is empty..!")
+        else:
+            temp = self.head # temp = first Node
+            while temp:
+                print(temp.data, end=" ")
+                temp = temp.next
+                
+obj = singleLinkedList()
+w = Node("W")
+obj.head = w
+i = Node("I")
+obj.head.next = i
+n1 = Node("N")
+i.next = n1
+n2 = Node("N")
+n1.next = n2
+e = Node("E")
+n2.next = e
+r = Node("R")
+e.next = r
+obj.display()
+
+
+
+'''Opeartions:'''
+# Inserting
+# Inserting at the Begining
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class singleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_begining(self, data):
+        new = Node(data)
+        new.next = self.head
+        self.head = new
+
+    def display(self):
+        if self.head is None:
+            print("The List is empty")
+        else:
+            temp = self.head
+            while temp:
+                print(temp.data, "->", end=" ")
+                temp = temp.next
+
+obj = singleLinkedList()
+n1 = Node(20)
+obj.head = n1
+n2 = Node(30)
+n1.next = n2
+n3 = Node(40)
+n2.next = n3
+n4 = Node(50)
+n3.next = n4
+print("List before insertig 10 at begining")
+obj.display()
+print("")
+print("List after insertig 10 at begining")
+obj.insert_begining(10)
+obj.display()
+
+
+
+# Inserting at the End
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class singleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_end(self, data):
+        new = Node(data)
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new
+
+    def display(self):
+        if self.head is None:
+            print("The List is empty")
+        else:
+            temp = self.head
+            while temp:
+                print(temp.data, "->", end=" ")
+                temp = temp.next
+
+obj = singleLinkedList()
+n1 = Node(10)
+obj.head = n1
+n2 = Node(20)
+n1.next = n2
+n3 = Node(30)
+n2.next = n3
+n4 = Node(40)
+n3.next = n4
+print("List before insertig 50 at end")
+obj.display()
+print("")
+print("List after insertig 50 at end")
+obj.insert_end(50)
+obj.display()
+
+
+
+# Inserting at the Postion
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class singleLinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def insert_position(self, pos, data):
+        new = Node(data)
+        temp = self.head
+        
+        for i in range(pos-1):
+            temp = temp.next
+            new.next = temp.next
+            temp.next = new
+        
+    def display(self):
+        if self.head is None:
+            print("The List is empty")
+        else:
+            temp = self.head
+            while temp:
+                print(temp.data, "->", end=" ")
+                temp = temp.next
+
+obj = singleLinkedList()
+n1 = Node(10)
+obj.head = n1
+n2 = Node(20)
+n1.next = n2
+n3 = Node(40)
+n2.next = n3
+n4 = Node(50)
+n3.next = n4
+print("List before insertig 30 at index 2")
+obj.display()
+print("")
+print("List after insertig 30 at index 2")
+obj.insert_position(2, 30)
+obj.display()
