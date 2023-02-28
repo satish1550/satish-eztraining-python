cecfc756e23da804ac4e7ef143c7a3bc1c01d852
Satish1550 <satishsunny1550@gmail.com>
2023-02-28 21:35:38 +0530

    DAY 2

--- Technical Training Phase II/Day2.py
+++ Technical Training Phase II/Day2.py
@@ -244,3 +244,33 @@ print("")
 print("List after inserting 30 at index 2")
 obj.insert_position(2, 30)
 obj.display()
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.last_node = None
+
+    def append(self, data):
+        if self.last_node is None:
+            self.head = Node(data)
+            self.last_node = self.head
+
+        else:
+            self.last_node.next = Node(data)
+            self.last_node = self.last_node.next
+
+    def display(self):
+        current = self.head
+        while current is not None:
+            print(current.data, end=' ')
+            current = current.next
+
+
+List = LinkedList()
+n = int(input("How many elements you like to ...?"))
+for i in range(n):
+    data = int(input("Enter data item :"))
+    List.append(data)
+print("The linked list is ", end=" ")
+List.display()
